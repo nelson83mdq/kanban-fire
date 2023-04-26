3ac104da7653589f1944c21e57559316395ffd13
Nelson Damian Brizuela <nelson83mdq@gmail.com>
2023-04-25 23:29:11 -0300

    adding task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'kanban-fire';
+  todo: Task[] = [
+    {
+      title: 'Buy Milk',
+      description: 'Go to the store and buy milk'
+    },
+    {
+      title: 'Create a Kanban App',
+      description: 'Using Firebase and Angular Materials, i Create a Kanban App!'
+    }
+  ] ;
 }
